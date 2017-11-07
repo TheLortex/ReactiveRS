a08dfb5c2be0899bf12f5c85ff57f69a83367e44
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2017-11-07 10:03:22 +0100

    value implem

--- src/engine.rs
+++ src/engine.rs
@@ -1,5 +1,36 @@
 use std;
 
+/// A reactive process.
+pub trait Process: 'static {
+    /// The value created by the process.
+    type Value;
+
+    /// Executes the reactive process in the runtime, calls `next` with the resulting value.
+    fn call<C>(self, runtime: &mut Runtime, next: C) where C: Continuation<Self::Value>;
+
+
+}
+
+pub struct Value<V> {
+    value: V,
+}
+
+impl<V> Process for Value<V> where V: 'static {
+    type Value = V;
+
+    fn call<C>(self, runtime: &mut Runtime, next: C) where C: Continuation<Self::Value> {
+        next.call(runtime, self.value);
+    }
+}
+
+impl<V> Value<V> where V: 'static{
+    fn new(v: V) -> Self {
+        Value {value: v}
+    }
+}
+
+
+
 /// A reactive continuation awaiting a value of type `V`. For the sake of simplicity,
 /// continuation must be valid on the static lifetime.
 pub trait Continuation<V>: 'static {
